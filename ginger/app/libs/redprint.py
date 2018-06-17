b616193bd636e50f72fd0a543c6ae670b2d48690
NanaCode <2503605326@qq.com>
2018-06-17 19:26:18 +0800

    edit class of Redprint

--- ginger/app/libs/redprint.py
+++ ginger/app/libs/redprint.py
@@ -6,4 +6,18 @@ __date__ = '2018/6/17 17:21'
 class RedPrint:
     def __init__(self, name):
         self.name = name
+        self.mound = []
+
+    def route(self, rule, **options):
+        def decorator(f):
+            self.mound.append((f, rule, options))
+            return f
+        return decorator
+
+    def register(self, bp, url_prefix=None):
+        for f, rule, options in self.mound:
+            endpoint = options.pop("endpoint", f.__name__)  # pythonic写法
+            bp.add_url_rule(url_prefix+rule, endpoint, f, **options)
+
+
 
