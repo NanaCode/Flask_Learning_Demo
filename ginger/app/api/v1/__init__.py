9a92eae2803f943b63f48e7abad5f80dfcd82824
NanaCode <2503605326@qq.com>
2018-06-17 22:43:55 +0800

    edit create client func

--- ginger/app/api/v1/__init__.py
+++ ginger/app/api/v1/__init__.py
@@ -2,7 +2,7 @@
 from flask import Blueprint
 # from app.api.v1.book import api as book
 # from app.api.v1.user import api as user
-from app.api.v1 import user, book
+from app.api.v1 import user, book, client
 
 __author__ = 'Nana'
 __date__ = '2018/6/17 16:59'
@@ -14,4 +14,5 @@ def create_blueprint_v1():
     # book.api.register(bp_v1, url_prefix='/book')
     user.api.register(bp_v1)
     book.api.register(bp_v1)
+    client.api.register(bp_v1)
     return bp_v1
--- ginger/app/api/v1/client.py
+++ ginger/app/api/v1/client.py
@@ -1,18 +1,35 @@
 # _*_ coding: utf-8 _*_
+from app.libs.enums import ClientTypeEnum
+
 __author__ = 'Nana'
 __date__ = '2018/6/17 21:20'
 
 from app.libs.redprint import RedPrint
+from app.validators.forms import ClientForm
 
 api = RedPrint('client')
 
 
-@api.route('/register')
+@api.route('/register', method=['POST'])
 def create_client():
     """
     用户客户端注册 登录
     参数 校验 接收参数
     WTForms 参数校验 验证表单
+    表单和JSON对象提交过来的参数的区别： 表单-网页 json-移动端
+    客户端提交数据的方式有很多种
     :return:
     """
+    # request.json
+    # request.args.to_dict()
+    data = request.json
+    form = ClientForm(data=data)  # 注意，json形式需要data=data
+    if form.validate():
+        promise = {
+            ClientTypeEnum.USER_EMAIL: __register_user_by_email
+        }
+    pass
+
+
+def __register_user_by_email():
     pass
